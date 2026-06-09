3255a8239d584c621365387c466afaafa7a90c9c
saima-231 <saimasultana6112015@gmail.com>
2026-06-09 10:49:30 +0600

    YouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,18 @@
+interface Clickable {
+    void onClick();
+}
+
+class Button implements Clickable {
+    @Override
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        Button btn = new Button();
+        btn.onClick();
+    }
+}
 
