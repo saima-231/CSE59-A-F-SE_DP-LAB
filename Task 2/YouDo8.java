78831373430928b68974e05e8b0dd42ff2ba63aa
saima-231 <saimasultana6112015@gmail.com>
2026-06-09 11:02:37 +0600

    YouDo8.java

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,38 @@
+class Item {
+    String name;
+    double price;
+
+    Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+    Item item1;
+    Item item2;
+
+    Order(Item item1, Item item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+    void printBill() {
+        double total = item1.price + item2.price;
+
+        System.out.println("Item 1: " + item1.name + " -$" + item1.price);
+        System.out.println("Item 2: " + item2.name + " - $" + item2.price);
+        System.out.println("Total Cost: $" + total);
+    }
+}
+
+public class Main8 {
+    public static void main(String[] args) {
+        Item mouse = new Item("Mouse", 20.0);
+        Item keyboard = new Item("Keyboard", 45.0);
+
+        Order order = new Order(mouse, keyboard);
+        order.printBill();
+    }
+}
 
