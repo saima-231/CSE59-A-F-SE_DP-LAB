646522c2175059878298409a0490872e2de50428
saima-231 <saimasultana6112015@gmail.com>
2026-06-09 10:47:44 +0600

    YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,19 @@
+class Printer {
+    void printData(String text) {
+        System.out.println(text);
+    }
+
+    void printData(int number) {
+        System.out.println(number);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.printData("Hello Java");
+        printer.printData(100);
+    }
+
+}
 
