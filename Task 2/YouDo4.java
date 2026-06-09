cc553e4fffc00c37e47d6c060d5846b77a186a0f
saima-231 <saimasultana6112015@gmail.com>
2026-06-09 10:46:26 +0600

    YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,23 @@
+class Sensor {
+    String name;
+    int batteryLevel;
+
+    Sensor(String name, int batteryLevel) {
+        this.name = name;
+        this.batteryLevel = batteryLevel;
+    }
+
+    void charge() {
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        Sensor sensor = new Sensor("Temperature Sensor", 20);
+        sensor.charge();
+        System.out.println("Battery Level: " + sensor.batteryLevel);
+    }
+}
 
