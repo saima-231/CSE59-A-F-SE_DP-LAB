e8866f7da56b882429f0b5de227972df61d80b20
saima-231 <saimasultana6112015@gmail.com>
2026-06-16 09:55:25 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,48 @@
+class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+class EmailValidator {
+
+    public static boolean isValid(String email) {
+        if (email != null && email.contains("@")) {
+            return true;
+        }
+
+        System.out.println("Invalid email format.");
+        return false;
+    }
+}
+
+class UserRepository {
+
+    public void save(User user) {
+        System.out.println("Connecting to database...");
+        System.out.println("Saving user " + user.getUsername() + " to the users table.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("john_doe", "john@example.com");
+        if (EmailValidator.isValid(user.getEmail())) {
+            UserRepository repository = new UserRepository();
+            repository.save(user);
+        }
+    }
+}
 
