4a576c170101397fba530239875c1592b2b8a9e2
saima-231 <saimasultana6112015@gmail.com>
2026-06-23 11:28:53 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,37 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending EMAIL: " + message);
+    }
+}
+
+class SMSSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender messageSender;
+
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        NotificationService emailService = new NotificationService(new EmailSender());
+        emailService.alertUser("Server is down!");
+        NotificationService smsService = new NotificationService(new SMSSender());
+        smsService.alertUser("Server is down!");
+    }
+}
 
