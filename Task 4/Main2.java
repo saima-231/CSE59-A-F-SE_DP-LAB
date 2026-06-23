758fddaf95e47353c5b03237d68b76a8d09fcfb1
saima-231 <saimasultana6112015@gmail.com>
2026-06-23 10:29:53 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,46 @@
+interface BonusEligible {
+    void calculateBonus();
+}
+
+class TeamMember {
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Employee extends TeamMember implements BonusEligible {
+    public Employee(String name) {
+        super(name);
+    }
+
+    @Override
+    public void calculateBonus() {
+        System.out.println(name + ": Calculating employee bonus ...");
+    }
+}
+
+class Contractor extends TeamMember {
+    public Contractor(String name) {
+        super(name);
+    }
+
+    public void submitInvoice() {
+        System.out.println(name + ": Submitting contractor invoice ...");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice");
+        emp.calculateBonus();
+        Contractor contractor = new Contractor("Bob");
+        contractor.submitInvoice();
+    }
+}
 
