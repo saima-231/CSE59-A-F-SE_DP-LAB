7a65c3bcbdd41ed2f1844d41365a9232cb7b5af1
saima-231 <saimasultana6112015@gmail.com>
2026-06-30 10:46:09 +0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,43 @@
+class Patient {
+    private String patientName;
+    private String nationId;
+
+    public Patient(String patientName, String nationId) {
+        this.patientName = patientName;
+        this.nationId = nationId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getNationId() {
+        return nationId;
+    }
+}
+
+class IdValidator {
+    public boolean validate(String nationId) {
+        return nationId.length() == 10 || nationId.length() == 17;
+    }
+}
+
+class SmsService {
+    public void sendConfirmation(Patient patient, IdValidator validator) {
+        if (validator.validate(patient.getNationId())) {
+            System.out.println("Sending SMS to " + patient.getPatientName() + ": Registration successful.");
+        } else {
+            System.out.println("Invalid National ID.");
+        }
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        Patient patient = new Patient("Rahim", "1234567890");
+        IdValidator validator = new IdValidator();
+        SmsService smsService = new SmsService();
+        smsService.sendConfirmation(patient, validator);
+    }
+}
 
