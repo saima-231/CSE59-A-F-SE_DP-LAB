d934ec3c7f5245d0c21db5e4eeab521f923ff841
saima-231 <saimasultana6112015@gmail.com>
2026-06-30 10:47:05 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,39 @@
+class UniversityMember {
+    public void attendLecture() {
+        System.out.println("Attending a lecture.");
+    }
+}
+
+interface Researcher {
+    void conductLabResearch();
+}
+
+class UndergraduateStudent extends UniversityMember {
+    // No research responsibility
+}
+
+class GraduateResearchAssistant extends UniversityMember implements Researcher {
+    @Override
+    public void conductLabResearch() {
+        System.out.println("Running experiments and publishing papers.");
+    }
+}
+
+class Professor extends UniversityMember implements Researcher {
+    @Override
+    public void conductLabResearch() {
+        System.out.println("Leading advanced research projects.");
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        UniversityMember student = new UndergraduateStudent();
+        student.attendLecture();
+        Researcher gra = new GraduateResearchAssistant();
+        gra.conductLabResearch();
+        Researcher professor = new Professor();
+        professor.conductLabResearch();
+    }
+}
 
