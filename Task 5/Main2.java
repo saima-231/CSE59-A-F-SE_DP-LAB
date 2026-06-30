bd97325e18753eaac5866524ff4128d9a74dbdfc
saima-231 <saimasultana6112015@gmail.com>
2026-06-30 10:45:46 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,31 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alterUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService service = new NotificationService(sender);
+        service.alterUser("System Alert!");
+    }
+}
 
