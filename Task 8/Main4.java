56674307d8ca12845ef488e281458a2bf6c27718
saima-231 <saimasultana6112015@gmail.com>
2026-07-21 11:04:08 +0600

    Update Main4.java

--- Task 8/Main4.java
+++ Task 8/Main4.java
@@ -1,1 +1,29 @@
+import java.lang.module.Configuration;
+
+class ConfigurationManager {
+    private static volatile ConfigurationManager instance;
+
+    private ConfigurationManager() {
+        System.out.println("Reading heavy config file from disk... (Only happens once)");
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (instance == null) {
+            synchronized (ConfigurationManager.class) {
+                if (instance == null) {
+                    instance = new ConfigurationManager();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        ConfigurationManager config1 = ConfigurationManager.getInstance();
+        ConfigurationManager config2 = ConfigurationManager.getInstance();
+        System.out.println("Same instance in memory? " + (config1 == config2));
+    }
+}
 
