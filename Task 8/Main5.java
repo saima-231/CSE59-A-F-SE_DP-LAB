d4e32e2e7f3e126ea051b40c7ba63951d19f363f
saima-231 <saimasultana6112015@gmail.com>
2026-07-21 11:05:41 +0600

    Update Main5.java

--- Task 8/Main5.java
+++ Task 8/Main5.java
@@ -1,1 +1,33 @@
+class ModelWeightsCache {
+    private static volatile ModelWeightsCache instance;
+
+    private ModelWeightsCache() {
+        System.out.println("Loading 2GB model weights into memory... (Cached successfully)");
+    }
+
+    public static ModelWeightsCache getInstance() {
+        if (instance == null) {
+            synchronized (ModelWeightsCache.class) {
+                if (instance == null) {
+                    instance = new ModelWeightsCache();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void predict(String dataSample) {
+        System.out.println("Running inference on: " + dataSample);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        ModelWeightsCache cache1 = ModelWeightsCache.getInstance();
+        cache1.predict("image_01.png");
+
+        ModelWeightsCache cache2 = ModelWeightsCache.getInstance();
+        cache2.predict("image_02.png"); // Uses the already loaded model
+    }
+}
 
